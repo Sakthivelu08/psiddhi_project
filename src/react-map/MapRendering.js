9ca33c8abbbaf9a814c2dcca42eaba03247e0b0a
Sakthivelu08 <your-email@example.com>
2024-11-23 13:01:04 +0530

    first commit

---
+++ src/react-map/MapRendering.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+import ReactMapGL from "./React_Map_Gl";
+
+function MapRendering() {
+    return(
+        <Box>
+            Hello
+            <ReactMapGL />
+        </Box>
+    );
+}
+
+export default MapRendering;
---
+++ src/react-map/React_Map_Gl.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Map from "react-map-gl/dist/esm/components/map";
+
+function ReactMapGL() {
+
+    return (
+        <Map
+            
+        />
+    );
+}
+
+export default ReactMapGL;
